25b10e9f1a44b5a60b9396ae0826c57f0db52809
Andrea Payne <anoelpayne@gmail.com>
2020-11-01 14:16:57 -0600

    Part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Cool Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images.pexels.com/photos/5483420/pexels-photo-5483420.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+  image3 = 'https://images.pexels.com/photos/5255412/pexels-photo-5255412.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
 
   constructor() { }
 
